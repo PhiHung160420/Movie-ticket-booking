268f3dc550683ff2fb044bce634ba967d0c04ca3
PhiHung160420 <67174257+PhiHung160420@users.noreply.github.com>
2021-05-14 22:57:09 +0700

    change name some file

--- app.js
+++ app.js
@@ -10,35 +10,34 @@ app.use(express.json());
 
 //express ejs-layouts
 app.use(expressLayouts);
-app.set('layout', './layouts/layout');
+app.set("layout", "./layouts/layout");
 
 //use ejs
 app.set("view engine", "ejs");
 
-app.set("views", "./client/views");
+app.set("views", "./users/views");
 
 //use body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //khai báo static file
-app.use('/User', express.static(__dirname + '/public'));
+app.use("/User", express.static(__dirname + "/public"));
 
 //use router for admin
-app.use("/admin", require("./server/routes/router"));
+app.use("/admin", require("./admin/routes/router"));
 
 //use router for user
-app.use("/user", require("./client/routes/home"));
-app.use("/user", require("./client/routes/movie-checkout"));
-app.use("/user", require("./client/routes/movie-customer"));
-app.use("/user", require("./client/routes/movie-detail"));
-app.use("/user", require("./client/routes/movie-list"));
-app.use("/user", require("./client/routes/movie-seat-plan"));
-app.use("/user", require("./client/routes/movie-ticket-plan"));
-app.use("/user", require("./client/routes/movie-contact")); 
-app.use("/user", require("./client/routes/sign-in"));
-app.use("/user", require("./client/routes/sign-up")); 
-
+app.use("/user", require("./users/routes/home"));
+app.use("/user", require("./users/routes/movie-checkout"));
+app.use("/user", require("./users/routes/movie-customer"));
+app.use("/user", require("./users/routes/movie-detail"));
+app.use("/user", require("./users/routes/movie-list"));
+app.use("/user", require("./users/routes/movie-seat-plan"));
+app.use("/user", require("./users/routes/movie-ticket-plan"));
+app.use("/user", require("./users/routes/movie-contact"));
+app.use("/user", require("./users/routes/sign-in"));
+app.use("/user", require("./users/routes/sign-up"));
 
 app.listen(process.env.PORT || 3000, function () {
   console.log(
