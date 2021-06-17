b73e91015c8d386fd5b5e48cbad7691263845e9a
PhiHung160420 <67174257+PhiHung160420@users.noreply.github.com>
2021-06-17 17:53:57 +0700

    done show statistic by theater cluster on chart

--- src/admin/controllers/home.js
+++ src/admin/controllers/home.js
@@ -1,7 +1,7 @@
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 const db = require("../../config/database/db");
 const asyncHandler = require("express-async-handler");
-const moment = require('moment');
+const moment = require("moment");
 const Theater_clusters = require("../../models/theater_clusters");
 const Movies = require("../../models/movie");
 const Ticket = require("../../models/ticket");
@@ -12,54 +12,53 @@ const Theater = require("../../models/theater");
 exports.getIndex = asyncHandler(async (req, res) => {
   //get list theater_clusters
   res.locals.lstTheaterCluster = await Theater_clusters.findAll({
-    attributes: [[db.fn("DISTINCT", db.col("name")), "name"], 'id'],
+    attributes: [[db.fn("DISTINCT", db.col("name")), "name"], "id"],
   });
 
   //get list movies
   res.locals.lstMovies = await Movies.findAll({
-    attributes: [[db.fn("DISTINCT", db.col("name")), "name"], 'id'],
+    attributes: [[db.fn("DISTINCT", db.col("name")), "name"], "id"],
   });
   res.render("admin/index");
 });
 
 //post statistics theater cluster
 exports.postStatisticCluster = asyncHandler(async (req, res) => {
-    const date_start_cluster = req.body.date_start;
-    const date_end_cluster = req.body.date_end;
-    const select_cluster = req.body.select_cluster;
-    
+  const {date_start, date_end, select_cluster} = req.body;
+  
+  if (select_cluster !== "all") {
     //get list theater by theater cluster id
     const getListTheater = await Theater.findAll({
-      attributes: ['id', 'name'],
+      attributes: ["id", "name"],
       where: {
-        theater_cluster_id: select_cluster
-      }
+        theater_cluster_id: select_cluster,
+      },
     });
 
     const listTheater = [];
-    getListTheater.forEach(item => listTheater.push(item.id));
+    getListTheater.forEach((item) => listTheater.push(item.id));
 
     const listBooking = await Booking.findAll({
       attributes: [
-        [db.fn('COUNT', db.col('booking.id')), 'AmountTicket'],
-        [db.fn('SUM', db.col('booking_price')), 'TotalPrice']
+        [db.fn("COUNT", db.col("booking.id")), "AmountTicket"],
+        [db.fn("SUM", db.col("booking_price")), "TotalPrice"],
       ],
       include: [
         {
           model: Showtimes,
           where: {
-            theater_id: listTheater
+            theater_id: listTheater,
           },
-          attributes: []
-        }
+          attributes: [],
+        },
       ],
       where: {
         booking_time: {
-          [Op.between]: [date_start_cluster, date_end_cluster]
-        }
+          [Op.between]: [date_start, date_end],
+        },
       },
-      group: ['booking.user_id'],
-      raw:true
+      group: ["booking.user_id"],
+      raw: true,
     });
 
     const lstResult = {};
@@ -72,8 +71,90 @@ exports.postStatisticCluster = asyncHandler(async (req, res) => {
       price += parseFloat(listBooking[i].TotalPrice);
     }
 
-    lstResult.ticketTotal = String(ticket).replace(/(.)(?=(\d{3})+$)/g,'$1,');
-    lstResult.priceTotal = price.toLocaleString('vi-VN', {style : 'currency', currency : 'VND'});
+    lstResult.ticketTotal = String(ticket).replace(/(.)(?=(\d{3})+$)/g, "$1,");
+    lstResult.priceTotal = price.toLocaleString("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    });
 
     return res.status(200).json(lstResult);
+  }
+  else
+  {
+    const listBooking = await Booking.findAll({
+      attributes: [
+        [db.fn("COUNT", db.col("booking.id")), "AmountTicket"],
+        [db.fn("SUM", db.col("booking_price")), "TotalPrice"],
+      ],
+      include: [
+        {
+          model: Showtimes,
+          as: 'showtime',
+          required: true,
+          attributes: [],
+          include: [
+            {
+              model: Theater,
+              as: 'theater',
+              required: true,
+              attributes: [],
+              include: [
+                {
+                  model: Theater_clusters,
+                  as: 'theater_cluster',
+                  required: true,
+                  attributes: ['name']
+                }
+              ]
+            }
+          ]
+        },
+      ],
+      where: {
+        booking_time: {
+          [Op.between]: [date_start, date_end],
+        },
+      },
+      group: ["showtime.theater.theater_cluster.name", "showtime.theater.theater_cluster.id"],
+      raw: true,
+    });
+
+    let lstResult = {};
+
+    let ticket = 0;
+    let price = 0;
+
+    for (var i = 0; i < listBooking.length; i++) {
+      ticket += parseInt(listBooking[i].AmountTicket);
+      price += parseFloat(listBooking[i].TotalPrice);
+    }
+
+    ticketTotal = String(ticket).replace(/(.)(?=(\d{3})+$)/g, "$1,");
+    priceTotal = price.toLocaleString("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    });
+
+    //get list labels
+    let listLabels=[];
+    listBooking.forEach(item => listLabels.push(item['showtime.theater.theater_cluster.name']));
+
+    //get list amount ticket
+    let listTicket = [];
+    listBooking.forEach(item => listTicket.push(item.AmountTicket));
+
+    //get list total price
+    let listPrice = [];
+    listBooking.forEach(item => listPrice.push(item.TotalPrice));
+
+    lstResult = {
+      listLabels,
+      listTicket,
+      listPrice,
+      ticketTotal,
+      priceTotal,
+    }
+
+    return res.status(200).json(lstResult);
+  }
 });
