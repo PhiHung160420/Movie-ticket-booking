f0ac27ed8bcecd394abaceb8323393c659b6dd10
PhiHung160420 <67174257+PhiHung160420@users.noreply.github.com>
2021-07-01 14:54:59 +0700

    change menu in layout user

--- src/users/controllers/movie-list.js
+++ src/users/controllers/movie-list.js
@@ -15,52 +15,51 @@ exports.listMovies = asyncHandler(async (req, res) => {
 exports.ListMoviesSort = asyncHandler(async (req, res) => {
     const sort = req.params.sort;
     console.log(sort);
-    if (sort === "releaseDate") {
+    if (sort === "releaseDate") 
+    {
       console.log('into releaseDate');
       const data = await Movies.findAll({
         order: [["releaseDate", "DESC"]],
       });
+
       data.forEach( movie => {
         if(movie.poster)
         {
           movie.poster = Buffer.from(movie.poster, 'binary').toString('base64');
         }
       });
+      
       console.log(data);
       res.render("users/movie-list", { movies: data });
     }
-    if (sort === "mostViewed") {
+    if (sort === "mostViewed") 
+    {
       const data = await Movies.findAll({
         order: [["viewed", "DESC"]],
       });
+
       data.forEach( movie => {
         if(movie.poster)
         {
           movie.poster = Buffer.from(movie.poster, 'binary').toString('base64');
         }
       });
+
       res.render("users/movie-list", { movies: data });
     }
     if (sort === "mostLiked") {
       const data = await Movies.findAll({
         order: [["liked", "DESC"]],
       });
+
       data.forEach( movie => {
         if(movie.poster)
         {
           movie.poster = Buffer.from(movie.poster, 'binary').toString('base64');
         }
       });
+
       res.render("users/movie-list", { movies: data });
-    } else {
-      const data = await Movies.findAll();
-      data.forEach( movie => {
-        if(movie.poster)
-        {
-          movie.poster = Buffer.from(movie.poster, 'binary').toString('base64');
-        }
-      });
-      res.render("users/movie-list", { movies: data });
-    }
+    } 
 });
 
