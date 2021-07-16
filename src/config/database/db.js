98d9d6aa6261e8be22f5ff6d66b83573d536f700
PhiHung160420 <67174257+PhiHung160420@users.noreply.github.com>
2021-07-16 10:28:10 +0700

    config to deploy heroku

--- src/config/database/db.js
+++ src/config/database/db.js
@@ -1,6 +1,15 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(
-  process.env.DATABASE_URL ||
-    "postgres://postgres:1234@localhost:5432/Movie-ticket-booking"
+    process.env.DATABASE_URL ||
+        "postgres://postgres:1234@localhost:5432/Movie-ticket-booking",
+    {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    }
 );
